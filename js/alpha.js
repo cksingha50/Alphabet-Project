7589ade0ab897c59823839dd2c5adc9f6a87a349
cksingha50 <cksingha50@gmail.com>
2024-02-19 15:31:44 +0600

    game Over

--- js/alpha.js
+++ js/alpha.js
@@ -11,9 +11,71 @@
 // const playGround = document.getElementById('play-ground');
 // playGround.classList.remove('hidden');
 
+
+
+
+function buttonPress(event){
+    const playerPressed = event.key;
+
+    if(playerPressed === 'Escape'){
+        gameOver();
+    }
+    // console.log('player pressed', playerPressed);
+
+    //get the expected to press
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    // console.log(playerPressed,expectedAlphabet);
+
+    if(playerPressed === expectedAlphabet){
+        // console.log('you win');
+        const currentScore = getTextElementById('current-score'); 
+        const updatedScore = currentScore + 1;
+        setTextElementById('current-score', updatedScore);
+
+
+        // const currentScoreElement = document.getElementById('current-score');
+        // const currentScoreText = currentScoreElement.innerText;
+        // const currentScore = parseInt(currentScoreText);
+        // console.log(currentScore);
+       
+
+
+        
+        // currentScoreElement.innerText = newScore;
+
+        removeBackground(expectedAlphabet);
+        continueGame();
+    }else{
+
+        const currentLife = getTextElementById('current-life');
+        const updatedLife = currentLife - 1;
+        setTextElementById('current-life',updatedLife);
+
+        
+        if(updatedLife ===0){
+            // console.log('game Over !!')
+            gameOver();
+        }
+
+        // console.log('you lost');
+        // const lifeElement = document.getElementById('current-life');
+        // const lifeText = lifeElement.innerText;
+        // const life = parseInt(lifeText);
+
+        // const newLife = life - 1;
+        // lifeElement.innerText = newLife;
+
+    }
+
+}
+
+document.addEventListener('keyup', buttonPress);
+
 function continueGame(){
     const alphabet = randomAlphabet();
-    console.log('Your Alphabet :',alphabet)
+    // console.log('Your Alphabet :',alphabet)
 
     //set random generated alphabet
     const currentAlphabet = document.getElementById('current-alphabet');
@@ -24,6 +86,27 @@ function continueGame(){
 
 function play(){
     hideElement('home-screen');
+    hideElement('final-score');
     showElement('play-ground');
+
+    setTextElementById('current-life',5);
+    setTextElementById('current-score',0);
+
     continueGame();
+
+}
+
+function gameOver(){
+
+    hideElement('play-ground');
+    showElement('final-score');
+
+    const lastScore = getTextElementById('current-score');
+    console.log(lastScore);
+    setTextElementById('last-score',lastScore);
+
+
+    const currentAlpha = getElementText('current-alphabet');
+    removeBackground(currentAlpha);
+
 }
--- js/another.js
+++ js/another.js
@@ -14,23 +14,40 @@ function setBackgroundColor(elementId){
     element.classList.add('bg-orange-400');
 
 }
-
-
+function removeBackground(elementId){
+    const element =  document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
+function getTextElementById(elementId){
+    const element = document.getElementById(elementId);
+    const elementValueText = element.innerText;
+    const value = parseInt(elementValueText);
+    return value;
+}
+function setTextElementById(elementId, value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
+function getElementText(elementId){
+    const element = document.getElementById(elementId);
+    const text = element.innerText;
+    return text;
+}
 
 function randomAlphabet(){
     //get or create an alphabet array
 
     const alphabetString ='abcdefghijklmnopqrstuvwxyz';
     const alphabets = alphabetString.split('');
-    console.log(alphabets);
+    // console.log(alphabets);
 
 
     //get a random index between 0-25
     const randomNumber = Math.random() * 25;
     const index = Math.round(randomNumber);
-    console.log(index);
+    // console.log(index);
 
     const alphabet = alphabets[index];
-    console.log(index,alphabet)
+    // console.log(index,alphabet)
     return alphabet;
 }    
